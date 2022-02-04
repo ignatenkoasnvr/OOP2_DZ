ed9a834aa2ee94043a42891161a49b528a1f145d
ignatenkoas <ignatenkoasnvr@gmail.com>
2022-02-05 00:33:02 +0300

    2+- 3+

--- main.cpp
+++ main.cpp
@@ -31,7 +31,11 @@ public:
 		m_arr = new int[length];
 		m_length = length;
 	}
-
+	Array_int(const Array_int& original, int length)
+	{
+		m_arr = new int[length];
+		Array_int m_arr = original;
+	}
 	~Array_int() // деструктор
 	{
 		delete[] m_arr;
@@ -43,10 +47,13 @@ public:
 		for (int count = 0; count < m_length; ++count)
 			m_arr[count] = rand() % 10 + 1;
 	}
-	int getLength()
+	int getLength() const
 	{return m_length;}
 
-	void print()
+	int getValue(int index) 
+	{ return m_arr[index]; }
+
+	void print() const
 	{
 		for (int count = 0; count < m_length; ++count)
 			cout << m_arr[count] << " ";
@@ -65,6 +72,20 @@ public:
 		cout << endl;
 	}
 };
+void konkat(Array_int& arr1, Array_int& arr2)
+{
+	int* konkat_arr;
+	konkat_arr = new int[arr1.getLength() + arr2.getLength()];
+	for (int count = 0; count < arr1.getLength() + arr2.getLength(); count++)
+		if (count < arr1.getLength())
+			konkat_arr[count] = arr1.getValue(count);
+		else
+			konkat_arr[count] = arr2.getValue(count - arr1.getLength());
+	for (int count = 0; count < arr1.getLength() + arr2.getLength(); count++)
+		cout << konkat_arr[count] << " ";
+		cout << endl;
+
+}
 
 int main()
 {
@@ -73,7 +94,15 @@ int main()
 	arr1.fillRandom();
 	arr1.print();
 
-	arr1.plusOne();
+	//arr1.plusOne();
+	
+	Array_int arr2(5);
+	//arr2 = { arr1 };
+	cout << endl;
+	arr2.fillRandom();
+	arr2.print();
+
+	konkat(arr1, arr2);
 
 
 	return 0;
