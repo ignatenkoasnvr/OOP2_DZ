abba72e2e0a4eb688efc3cb80ca3d95d8ab8c478
ignatenkoas <ignatenkoasnvr@gmail.com>
2022-02-04 23:37:21 +0300

    1+-

--- main.cpp
+++ main.cpp
@@ -43,6 +43,8 @@ public:
 		for (int count = 0; count < m_length; ++count)
 			m_arr[count] = rand() % 10 + 1;
 	}
+	int getLength()
+	{return m_length;}
 
 	void print()
 	{
@@ -51,15 +53,27 @@ public:
 		cout << endl;
 	}
 
+	void plusOne()
+	{
+		int* m_arr_plusOne;
+		m_arr_plusOne = new int[m_length + 1];
+		for (int count = 0; count < m_length; count++)
+			m_arr_plusOne[count] = m_arr[count];
+
+		for (int count = 0; count < m_length+1; ++count)
+			cout << m_arr[count] << " ";
+		cout << endl;
+	}
 };
 
 int main()
 {
-
 	Array_int arr1(10);
 	arr1.print();
 	arr1.fillRandom();
 	arr1.print();
+
+	arr1.plusOne();
 
 
 	return 0;
